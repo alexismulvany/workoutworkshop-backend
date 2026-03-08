773d7f52d5bb01aa2372c8948d9f98b77ab55e20
Dylan Goble <dgoble0516@gmail.com>
2026-03-08 17:40:55 -0400

    Initial Setup

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ CORS(app)
 
 #DB Config
 # Replace 'root', 'password', and 'fitness_db' with your actual MySQL info
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password123@localhost/fitness_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/GroupProject'
 
 db = SQLAlchemy(app)
 
@@ -19,6 +19,21 @@ app.register_blueprint(auth_bp)
 app.register_blueprint(coach_bp)
 app.register_blueprint(user_bp)
 
+@app.route('/test-db')
+def test_db():
+    try:
+        # We use db.text() to run a raw SQL 'ping'
+        result = db.session.execute(db.text("SELECT 1")).fetchone()
+        return jsonify({
+            "status": "success",
+            "message": "Flask is talking to MySQL!",
+            "result": result[0]
+        }), 200
+    except Exception as e:
+        return jsonify({
+            "status": "error",
+            "message": str(e)
+        }), 500
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080, host='localhost')
